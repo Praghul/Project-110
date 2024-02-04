dac0fdca71c507737ec73924da046ab940017ec2
Praghul <113909116+Praghul@users.noreply.github.com>
2024-02-04 15:08:33 +0300

    Add files via upload

---
+++ game.py
@@ -0,0 +1,57 @@
+import random
+ans = input("Do you want to roll the dice YES or NO")
+
+if ans == "YES" or "Yes" or "yes":
+   no = random.randint(1,6)
+
+if no == 1:
+   print("[-----]")
+   print("[     ]")     
+   print("[  0  ]")  
+   print("[     ]")  
+   print("[-----]")
+   ans = input("Do you want to roll the dice YES or NO")  
+
+if no == 2:
+   print("[-----]")
+   print("[     ]")     
+   print("[ 0 0 ]")  
+   print("[     ]")  
+   print("[-----]") 
+   ans = input("Do you want to roll the dice YES or NO") 
+
+if no == 3:
+   print("[-----]")
+   print("[0    ]")     
+   print("[  0  ]")  
+   print("[    0]")  
+   print("[-----]")  
+   ans = input("Do you want to roll the dice YES or NO")
+
+if no == 4:
+   print("[-----]")
+   print("[ 0 0 ]")     
+   print("[     ]")  
+   print("[ 0 0 ]")  
+   print("[-----]")  
+   ans = input("Do you want to roll the dice YES or NO")
+
+if no == 5:
+   print("[-----]")
+   print("[ 0 0 ]")     
+   print("[  0  ]")  
+   print("[ 0 0 ]")  
+   print("[-----]")     
+   ans = input("Do you want to roll the dice YES or NO")         
+
+if no == 6:
+   print("[-----]")
+   print("[ 0 0 ]")     
+   print("[ 0 0 ]")  
+   print("[ 0 0 ]")  
+   print("[-----]")  
+   ans = input("Do you want to roll the dice YES or NO")
+
+
+
+
